366cc07542af151a60bf14ca582359437cd995ed
Gabriel Martínez <gamartinezayala@gmail.com>
2025-04-15 20:17:21 -0400

    Update PC animation

--- src/components/HeroCamera.jsx
+++ src/components/HeroCamera.jsx
@@ -18,11 +18,12 @@ const HeroCamera = ({ isMobile, children }) => {
   }, []);
 
   useFrame((state, delta) => {
-    const targetPosition = [-0.5, 0, 15.5 + scrollY * 0.06];
+    const scrollYOffset = scrollY < 840 ? scrollY : 840;
+    const targetPosition = [-0.5, scrollYOffset* -0.005, 15.5 + scrollYOffset * 0.02];
     easing.damp3(state.camera.position, targetPosition, 0.25, delta);
 
     // if (!isMobile) {
-      easing.damp3(group.current.rotation, [0, scrollY * 0.001, 0], 0.25, delta);
+      easing.damp3(group.current.rotation, [0, scrollYOffset * 0.0008, 0], 0.25, delta);
     // }
   });
 
