0a172e96990fd5b80bab7dee802f2ed60a2b03c8
Gabriel Martínez <gamartinezayala@gmail.com>
2026-01-16 18:35:40 -0400

    chore: Minor Footer component update

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-[#02270E] w-full pt-3 pb-3 border-t border-black-300 flex justify-center items-center">
-      <div className="text-white-500">
+      <div className="text-primary">
         <p>COPYRIGHT © 2025 Gabriel Martinez.</p>
       </div>
     </footer>
